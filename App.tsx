6924fd6078c49872098b19a885febf866441fdbe
lamine87 <laminenatidiarra@yahoo.fr>
2022-10-06 22:20:59 +0200

    Installation environnement

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bienvenue </Text>
+      <Image source={{  }}></Image>
       <StatusBar style="auto" />
     </View>
   );
